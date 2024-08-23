5e817f3d82113df71875d1a0b0903daf50a4b67b
Aaron B <aaronbo@bu.edu>
2024-08-23 10:31:27 -0400

    Switching to different article items

--- FS/Ts/build/item_testgun.d
+++ FS/Ts/build/item_testgun.d
@@ -23,6 +23,8 @@ D:\projects\Small-Arms_Melee\FS\Ts\build\item_testgun.o: \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/match.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/dynamics.h \
+ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/stage.h \
+ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/color.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/css.h \
@@ -33,8 +35,8 @@ D:\projects\Small-Arms_Melee\FS\Ts\build\item_testgun.o: \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/inline.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/mex.h \
- D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h \
+ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/preload.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/item.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/kirby.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h \
@@ -73,6 +75,8 @@ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/effects.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/match.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/dynamics.h:
+D:\projects\Small-Arms_Melee\m-ex\MexTK/include/stage.h:
+D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/color.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/css.h:
@@ -83,8 +87,8 @@ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/fighter.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/inline.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/mex.h:
-D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h:
+D:\projects\Small-Arms_Melee\m-ex\MexTK/include/preload.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/item.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/kirby.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h:
--- FS/Ts/build/test.d
+++ FS/Ts/build/test.d
@@ -23,6 +23,8 @@ D:\projects\Small-Arms_Melee\FS\Ts\build\test.o: \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/match.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/dynamics.h \
+ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/stage.h \
+ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/color.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/css.h \
@@ -33,8 +35,8 @@ D:\projects\Small-Arms_Melee\FS\Ts\build\test.o: \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/inline.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/mex.h \
- D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h \
+ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/preload.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/item.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/kirby.h \
  D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h \
@@ -79,6 +81,8 @@ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/effects.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/match.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/dynamics.h:
+D:\projects\Small-Arms_Melee\m-ex\MexTK/include/stage.h:
+D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/collision.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/color.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/css.h:
@@ -89,8 +93,8 @@ D:\projects\Small-Arms_Melee\m-ex\MexTK/include/fighter.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/inline.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/mex.h:
-D:\projects\Small-Arms_Melee\m-ex\MexTK/include/hsd.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h:
+D:\projects\Small-Arms_Melee\m-ex\MexTK/include/preload.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/item.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/kirby.h:
 D:\projects\Small-Arms_Melee\m-ex\MexTK/include/math.h:
